d149bc8fc7bcd235e9953ea7a6ae07c1f34eb69e
CaptainCredible <cpt.credible@gmail.com>
2019-04-02 11:27:44 +0200

    Add project files.

---
+++ I2C to MIDI/I2C to MIDI.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		I2C_to_MIDI.ino
+ Created:	4/2/2019 11:27:38 AM
+ Author:	Stoodio 2nd
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
